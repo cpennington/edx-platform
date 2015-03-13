c1abce7dc0cece4fedc9fda6ee47cd801ff93214
Calen Pennington <cale@edx.org>
2015-03-13 14:49:27 -0400

    WIP: allow for keyed ConfigurationModels

--- common/djangoapps/config_models/models.py
+++ common/djangoapps/config_models/models.py
@@ -23,6 +23,8 @@ class ConfigurationModel(models.Model):
     class Meta(object):  # pylint: disable=missing-docstring
         abstract = True
 
+    KEY_FIELDS = ()
+
     # The number of seconds
     cache_timeout = 600
 
@@ -35,28 +37,37 @@ class ConfigurationModel(models.Model):
         Clear the cached value when saving a new configuration entry
         """
         super(ConfigurationModel, self).save(*args, **kwargs)
-        cache.delete(self.cache_key_name())
+        cache.delete(self.cache_key_name(*[getattr(self, key) for key in self.KEY_FIELDS]))
 
     @classmethod
-    def cache_key_name(cls):
+    def cache_key_name(cls, *args):
         """Return the name of the key to use to cache the current configuration"""
-        return 'configuration/{}/current'.format(cls.__name__)
+        if cls.KEY_FIELDS != ():
+            if len(args) != len(cls.KEY_FIELDS):
+                raise TypeError("cache_key_name() takes exactly {} arguments ({} given)".format(len(cls.KEY_FIELDS), len(args)))
+            return 'configuration/{}/current/{}'.format(
+                cls.__name__,
+                ','.join(repr(arg) for arg in args)
+            )
+        else:
+            return 'configuration/{}/current'.format(cls.__name__)
 
     @classmethod
-    def current(cls):
+    def current(cls, *args):
         """
         Return the active configuration entry, either from cache,
         from the database, or by creating a new empty entry (which is not
         persisted).
         """
-        cached = cache.get(cls.cache_key_name())
+        cached = cache.get(cls.cache_key_name(*args))
         if cached is not None:
             return cached
 
+        key_dict = dict(zip(cls.KEY_FIELDS, args))
         try:
-            current = cls.objects.order_by('-change_date')[0]
+            current = cls.objects.filter(**key_dict).order_by('-change_date')[0]
         except IndexError:
-            current = cls()
+            current = cls(**key_dict)
 
-        cache.set(cls.cache_key_name(), current, cls.cache_timeout)
+        cache.set(cls.cache_key_name(*args), current, cls.cache_timeout)
         return current
--- common/djangoapps/config_models/tests.py
+++ common/djangoapps/config_models/tests.py
@@ -2,10 +2,10 @@
 Tests of ConfigurationModel
 """
 
+import ddt
 from django.contrib.auth.models import User
 from django.db import models
 from django.test import TestCase
-
 from freezegun import freeze_time
 
 from mock import patch
@@ -74,3 +74,93 @@ class ConfigurationModelTests(TestCase):
         ExampleConfig.current()
 
         mock_cache.set.assert_called_with(ExampleConfig.cache_key_name(), first, 300)
+
+
+class ExampleKeyedConfig(ExampleConfig):
+    """
+    Test model for testing ``ConfigurationModels`` with keyed configuration.
+    """
+
+    KEY_FIELDS = ('left', 'right')
+
+    left = models.CharField(max_length=30)
+    right = models.CharField(max_length=30)
+
+
+@ddt.ddt
+@patch('config_models.models.cache')
+class KeyedConfigurationModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User()
+        self.user.save()
+
+    @ddt.data(('a', 'b'), ('c', 'd'))
+    @ddt.unpack
+    def test_cache_key_name(self, left, right, _mock_cache):
+        self.assertEquals(ExampleKeyedConfig.cache_key_name(left, right), 'configuration/ExampleKeyedConfig/current/{!r},{!r}'.format(left, right))
+
+    @ddt.data(('a', 'b'), ('c', 'd'))
+    @ddt.unpack
+    def test_no_config_empty_cache(self, left, right, mock_cache):
+        mock_cache.get.return_value = None
+
+        current = ExampleKeyedConfig.current(left, right)
+        self.assertEquals(current.int_field, 10)
+        self.assertEquals(current.string_field, '')
+        mock_cache.set.assert_called_with(ExampleKeyedConfig.cache_key_name(left, right), current, 300)
+
+    @ddt.data(('a', 'b'), ('c', 'd'))
+    @ddt.unpack
+    def test_no_config_full_cache(self, left, right, mock_cache):
+        current = ExampleKeyedConfig.current(left, right)
+        self.assertEquals(current, mock_cache.get.return_value)
+
+    def test_config_ordering(self, mock_cache):
+        mock_cache.get.return_value = None
+
+        with freeze_time('2012-01-01'):
+            ExampleKeyedConfig(
+                changed_by=self.user,
+                left='left_a',
+                right='right_a',
+                string_field='first_a',
+            ).save()
+
+            ExampleKeyedConfig(
+                changed_by=self.user,
+                left='left_b',
+                right='right_b',
+                string_field='first_b',
+            ).save()
+
+        ExampleKeyedConfig(
+            changed_by=self.user,
+            left='left_a',
+            right='right_a',
+            string_field='second_a',
+        ).save()
+        ExampleKeyedConfig(
+            changed_by=self.user,
+            left='left_b',
+            right='right_b',
+            string_field='second_b',
+        ).save()
+
+        self.assertEquals(ExampleKeyedConfig.current('left_a', 'right_a').string_field, 'second_a')
+        self.assertEquals(ExampleKeyedConfig.current('left_b', 'right_b').string_field, 'second_b')
+
+    def test_cache_set(self, mock_cache):
+        mock_cache.get.return_value = None
+
+        first = ExampleKeyedConfig(
+            changed_by=self.user,
+            left='left',
+            right='right',
+            string_field='first',
+        )
+        first.save()
+
+        ExampleKeyedConfig.current('left', 'right')
+
+        mock_cache.set.assert_called_with(ExampleKeyedConfig.cache_key_name('left', 'right'), first, 300)
